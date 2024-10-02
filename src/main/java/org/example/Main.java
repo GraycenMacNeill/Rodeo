9a4dac45a5c2bdfc32441fc7083123132fdde0a5
Graycen MacNeill <graycenmacneill@gmail.com>
2024-10-01 21:39:00 -0700

    Added src

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package com.github.GraycenMacNeill.Rodeo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is John Doe");
+        System.out.println("I'm a software engineer");
+        System.out.println("I live in New York City");
+    }
+}
