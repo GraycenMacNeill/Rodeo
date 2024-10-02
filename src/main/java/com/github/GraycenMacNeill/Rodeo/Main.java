1fef41389a0f98791e6e03e8115a2b5ff42e9b97
Graycen MacNeill <graycenmacneill@gmail.com>
2024-10-01 21:46:26 -0700

    update

--- src/main/java/com/github/GraycenMacNeill/Rodeo/Main.java
+++ src/main/java/com/github/GraycenMacNeill/Rodeo/Main.java
@@ -2,10 +2,6 @@ package com.github.GraycenMacNeill.Rodeo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("My name is John Doe");
-        System.out.println("I'm a software engineer");
-        System.out.println("I live in New York City");
-        System.out.println("I enjoy coding in Java");
+        System.out.println("test!");
     }
 }
